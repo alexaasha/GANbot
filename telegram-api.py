aaff9e2ccb9c48764b6b32e564bbcd925d1232cd
Alex <caramboox@gmail.com>
2023-01-22 21:40:53 +0300

    Add interaction with telegram-bot

---
+++ telegram-api.py
@@ -0,0 +1,45 @@
+import logging
+import json
+from telegram import Update
+from inference import main
+from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, ContextTypes
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+
+async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
+
+
+async def get_photo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    new_file = await context.bot.get_file(update.message.photo[-1].file_id)
+    input_name = f"inputs/{abs(hash(update.message.photo[-1].file_id[:10]))}.jpeg"
+    await new_file.download_to_drive(custom_path=input_name)
+    main(input_name)
+    output_name = f"results/restored_imgs/{abs(hash(update.message.photo[-1].file_id[:10]))}.jpeg"
+    await context.bot.send_document(chat_id=update.effective_chat.id, document=output_name)
+
+
+if __name__ == '__main__':
+    with open('token.json', 'r') as f:
+        creds = json.load(f)
+
+    application = ApplicationBuilder().token(creds["token"]).build()
+
+    start_handler = CommandHandler('start', start)
+    application.add_handler(start_handler)
+
+    photo_handler = MessageHandler(filters.PHOTO, get_photo)
+    application.add_handler(photo_handler)
+
+    unknown_handler = MessageHandler(filters.COMMAND, unknown)
+    application.add_handler(unknown_handler)
+
+    application.run_polling()
