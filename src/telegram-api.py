f1c45cc2ba7a3a6befbbbd6bc32dabf497a852b9
Alex <caramboox@gmail.com>
2023-01-24 23:06:28 +0300

    Perform small fixes

--- src/telegram-api.py
+++ src/telegram-api.py
@@ -1,17 +1,31 @@
-import logging
 import json
-from telegram import Update
+import logging
+
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, ContextTypes, CallbackQueryHandler
+
 from inference import main
-from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, ContextTypes
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
 
+example_text = "/example"
+
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+    buttons = [[InlineKeyboardButton("example", callback_data=example_text)]]
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, I can improve quality of"
+                                                                          " faces on photo! Press button to get "
+                                                                          "an example.",
+                                   reply_markup=InlineKeyboardMarkup(buttons))
+
+
+async def get_example(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    example_pic = 'inputs/example.jpg'
+    if example_text in update.callback_query.data:
+        await context.bot.send_document(chat_id=update.effective_chat.id, document=example_pic)
 
 
 async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -22,9 +36,9 @@ async def get_photo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     new_file = await context.bot.get_file(update.message.photo[-1].file_id)
     image_hash = abs(hash(update.message.photo[-1].file_id))
     input_name = f"{image_hash}.jpeg"
-    await new_file.download_to_drive(custom_path=f"inputs/{image_hash}.jpeg")
+    await new_file.download_to_drive(custom_path=f"inputs/{input_name}")
     main(input_name)
-    output_name = f"results/restored_imgs/{image_hash}.jpeg"
+    output_name = f"results/restored_imgs/{input_name}"
     await context.bot.send_document(chat_id=update.effective_chat.id, document=output_name)
 
 
@@ -35,6 +49,9 @@ application = ApplicationBuilder().token(creds["token"]).build()
 
 start_handler = CommandHandler('start', start)
 application.add_handler(start_handler)
+
+example_handler = CallbackQueryHandler(get_example)
+application.add_handler(example_handler)
 
 photo_handler = MessageHandler(filters.PHOTO, get_photo)
 application.add_handler(photo_handler)
